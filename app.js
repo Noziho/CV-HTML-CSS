6de26b2e09b6e7a7b20cd85e316804ec23963c4e
noahd <noah.decroix3@gmail.com>
2022-08-12 22:27:58 +0200

    label event done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const h1 = document.querySelector("h1");
 let h2 = document.querySelectorAll("h2");
 let section = document.querySelectorAll("section");
-let label = document.querySelectorAll("label");
+let labelLetters = document.querySelectorAll("span.letters");
+const colors = ["red", "yellow", "green", "gray", "rebeccapurple", "purple", "aqua", "blue", "bisque", "beige"];
+const fontFamily = ["Agency FB", "Alef", "Algerian", "Amiri", "Amiri Quran", "Arial", "Arial Black", "Arial Narrow"];
 
 h1.animate([
         {
@@ -46,8 +48,22 @@ section.forEach(function (section) {
 
 })
 
-label.forEach(function (label) {
-    let table = label.innerHTML.split('');
+labelLetters.forEach(function (letters) {
+    letters.addEventListener("mouseover", function () {
+        letters.style.color = random(colors);
+        letters.style.fontFamily = random(fontFamily);
+        setTimeout(function () {
+            letters.style.color = "black";
+            letters.style.fontFamily = "";
+        }, 300);
+    })
 
 
 })
+
+function random (table) {
+    let random = Math.trunc(Math.random() * table.length);
+    return table[random];
+}
+
+
